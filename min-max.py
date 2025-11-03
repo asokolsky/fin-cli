c7053fd3d3a79ca505aee3470027a0008b190a9c
Alex Sokolsky <asokolsky@gmail.com>
2025-11-02 17:17:21 -0800

    feat: console app

--- min-max.py
+++ min-max.py
@@ -3,11 +3,12 @@ import sys
 
 # import time
 from argparse import ArgumentParser, RawTextHelpFormatter
-from pathlib import Path
 from typing import Any
 
 import yfinance as yf
 from tabulate import tabulate
+
+from tickers import analyze_ticker, headers, load_tickers
 
 logger = logging.getLogger(__name__)
 
@@ -21,44 +22,6 @@ epilog = """Examples:
 """
 
 
-def load_tickers(fname: str) -> set[str]:
-    """
-    Load tickers from file fname
-    """
-    tickers: set[str] = set()
-    path: Path = Path(fname)
-    with path.open(encoding='utf-8') as f:
-        for line1 in f:
-            line = line1.strip()
-            if line and not line.startswith('#'):
-                tickers.add(line)
-    return tickers
-
-def analyze_ticker(ticker: yf.Ticker) -> list[str]:
-    recommendations = []
-    high_low_proximity_percent = 20
-    info = ticker.info
-    fifty_two_week_high = info.get('fiftyTwoWeekHigh')
-    currentPrice = info.get('currentPrice')
-    # the highest price a buyer is ready to pay
-    bid = info.get('bid')
-    # the lowest price a seller is ready to accept
-    ask = info.get('ask')
-    fifty_two_week_low = info.get('fiftyTwoWeekLow')
-    dayLow = info.get('dayLow')
-    dayHigh = info.get('dayHigh')
-    yearly_range = fifty_two_week_high - fifty_two_week_low
-    assert yearly_range > 0
-    if dayHigh == fifty_two_week_high or bid > fifty_two_week_high:
-        recommendations.append('sell, 1y high')
-    elif bid > fifty_two_week_high - (yearly_range * high_low_proximity_percent / 100):
-        recommendations.append('sell, close to high')
-    if dayLow == fifty_two_week_low or ask < fifty_two_week_low:
-        recommendations.append('buy, 1y low')
-    elif ask < fifty_two_week_low + (yearly_range * high_low_proximity_percent / 100):
-        recommendations.append('buy, close to low')
-    return recommendations
-
 def process_tickers(logger: logging.Logger, tickers: set[str]) -> None:
     """
     Process tickers
@@ -67,14 +30,11 @@ def process_tickers(logger: logging.Logger, tickers: set[str]) -> None:
     tkrs.history(period='1d', repair=True, progress=False)
 
     # Define the headers for the table
-    headers = [
-        'TKR', 'Low1y', 'Low1d', 'Bid', 'Price', 'Ask', 'High1d', 'High1y',
-        'Change', 'Change %', 'Thoughts'
-    ]
+
     table_data = []
     for ticker in tkrs.tickers.values():
         info = ticker.info
-        #print(info)
+        # print(info)
         table_data.append(
             [
                 ticker.ticker,
@@ -89,7 +49,7 @@ def process_tickers(logger: logging.Logger, tickers: set[str]) -> None:
                 info.get('fiftyTwoWeekHigh'),
                 info.get('regularMarketChange'),
                 info.get('regularMarketChangePercent'),
-                '; '.join(analyze_ticker(ticker))
+                '; '.join(analyze_ticker(ticker)),
             ]
         )
 
---
+++ tickers.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import yfinance as yf
+
+headers = (
+    'TIKR',
+    'Low1y',
+    'Low1d',
+    'Bid',
+    'Price',
+    'Ask',
+    'High1d',
+    'High1y',
+    'Change',
+    'Change %',
+    'Thoughts'
+)
+
+
+def load_tickers(fname: str) -> set[str]:
+    """
+    Load tickers from file fname
+    """
+    tickers: set[str] = set()
+    path: Path = Path(fname)
+    with path.open(encoding='utf-8') as f:
+        for line1 in f:
+            line = line1.strip()
+            if line and not line.startswith('#'):
+                tickers.add(line)
+    return tickers
+
+
+def analyze_ticker(ticker: yf.Ticker) -> list[str]:
+    recommendations = []
+    high_low_proximity_percent = 20
+    info = ticker.info
+    fifty_two_week_high = info.get('fiftyTwoWeekHigh')
+    # currentPrice = info.get('currentPrice')
+    # the highest price a buyer is ready to pay
+    bid = info.get('bid')
+    # the lowest price a seller is ready to accept
+    ask = info.get('ask')
+    fifty_two_week_low = info.get('fiftyTwoWeekLow')
+    dayLow = info.get('dayLow')
+    dayHigh = info.get('dayHigh')
+    yearly_range = fifty_two_week_high - fifty_two_week_low
+    assert yearly_range > 0
+    if dayHigh == fifty_two_week_high or bid > fifty_two_week_high:
+        recommendations.append('sell, 1y high')
+    elif bid > fifty_two_week_high - (yearly_range * high_low_proximity_percent / 100):
+        recommendations.append('sell, close to high')
+    if dayLow == fifty_two_week_low or ask < fifty_two_week_low:
+        recommendations.append('buy, 1y low')
+    elif ask < fifty_two_week_low + (yearly_range * high_low_proximity_percent / 100):
+        recommendations.append('buy, close to low')
+    return recommendations
